493fde2bda229feb0e15beaf551abeefb31cb688
Adriana Palomino <adriana.palomino@tecsup.edu.pe>
2024-04-08 12:12:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const db = require('./db');
 const pedidos = require('./routes/pedidos');
 
 const path = __dirname + '/views/';
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 11899;
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
